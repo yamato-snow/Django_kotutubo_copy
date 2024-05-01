4007203320460394a8ac7999812912d3b96b942f
yamato-snow <yamato2016baseball@icloud.com>
2024-05-01 12:22:40 +0900

    5コマ目の3-9終了

--- project/helloworldproject/helloworldproject/urls.py
+++ project/helloworldproject/helloworldproject/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import helloworldfunc
+from .views import helloworldfunc, HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworldurl/', helloworldfunc),
+    path('helloworldurl2/', HelloWorldClass.as_view()),
 ]
